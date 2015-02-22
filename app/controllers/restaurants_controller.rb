43d57eb8f017910bba70b3b3161925d50a2e7c01
Danny Kulas <danny.kulas1@gmail.com>
2015-02-22 16:10:03 -0500

    updated rest and users controllers

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,10 @@
 class RestaurantsController < ApplicationController
+
+	def index
+		
+	end
+
+	def show
+		
+	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,31 @@
 class UsersController < ApplicationController
 	before_action :authenticate_user!
+
+	def index
+		
+	end
+
+	def show
+		
+	end
+
+	def new
+		
+	end
+
+	def create
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
 end
