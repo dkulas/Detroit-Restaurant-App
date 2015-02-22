46960f5963f9c28feaa587af225e0b9a79c28cbe
Danny Kulas <danny.kulas1@gmail.com>
2015-02-22 09:49:43 -0500

    updated migrations for devise and set up devise association for votes and comments

--- db/migrate/20150221225741_create_comments.rb
+++ db/migrate/20150221225741_create_comments.rb
@@ -1,6 +1,7 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+    	t.belongs_to :user
       t.integer :restaurant_id
       t.string :title
       t.string :body
--- db/migrate/20150221225810_create_votes.rb
+++ db/migrate/20150221225810_create_votes.rb
@@ -1,6 +1,7 @@
 class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
+    	t.belongs_to :user
       t.integer :restaurant_id
       t.integer :user_id
 
