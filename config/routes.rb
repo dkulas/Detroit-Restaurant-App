0138365d8837c6bebf621ad4acf16baa688784fb
Danny Kulas <danny.kulas1@gmail.com>
2015-02-22 16:13:37 -0500

    updated routes for restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "home#index"
+  root "restaurants#index"
 
   devise_for :users, :path => 'accounts'
 
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   	resources :comments
   end
 
-  resources :restaurants
+  resources :restaurants, only: [:index, :show]
 end
