b60d0f6762723de1140b22edddeff924b7ce8403
Danny Kulas <danny.kulas1@gmail.com>
2015-02-23 13:41:11 -0500

    used figaro gem to protect twitter keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "AeOTp9By88tnm8QJgQ814Is25", "2mmztxkY9dNw5u7BbAkQD7e1gpbOulzLcAlElfSFPQBCLdpyFv",
-  {
-  	:force_login => 'true'
-  }
+  provider :twitter, ENV["twitter_key"], ENV["twitter_secret"]
 end
