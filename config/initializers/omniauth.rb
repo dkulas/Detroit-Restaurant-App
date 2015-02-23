455a39d350b97177e30035e6c963e0f9c949b885
Danny Kulas <danny.kulas1@gmail.com>
2015-02-23 13:12:44 -0500

    added session controller and updated styling for app layout view and created omniauth rb file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "AeOTp9By88tnm8QJgQ814Is25", "2mmztxkY9dNw5u7BbAkQD7e1gpbOulzLcAlElfSFPQBCLdpyFv"
+  provider :twitter, "AeOTp9By88tnm8QJgQ814Is25", "2mmztxkY9dNw5u7BbAkQD7e1gpbOulzLcAlElfSFPQBCLdpyFv",
+  {
+  	:force_login => 'true'
+  }
 end
