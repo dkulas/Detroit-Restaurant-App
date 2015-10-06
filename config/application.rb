aa10b7a1ee6137f307fac9f4745f6e1599416964
Danny Kulas <danny.kulas1@gmail.com>
2015-10-06 13:32:49 -0400

    updated config file dynopoker config.address url

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module DscovrApp
 end
 
 Dynopoker.configure do |config|
-    config.address = 'http://wakemydyno.com'
+    config.address = 'https://cryptic-chamber-1899.herokuapp.com/'
 #  config.enable = false # default is true
 #  config.poke_frequency = 123 # default is 1800s (30min)
 end
